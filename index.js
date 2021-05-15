3bc772cedb116ccd1b9537277d712fb10d7a6441
fedeperin <federicoperin@outlook.com.ar>
2021-05-15 19:52:22 -0300

    Primer Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('someone connected')
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', msg);
+    });
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing');
+    });
+    socket.on('no typing', () => {
+        socket.broadcast.emit('no typing');
+    });
+    socket.on('disconnect', () => {
+      io.emit('someone disconnected')
+    });
+});
+
+server.listen(port, () => {
+  console.log('listening on' + port);
+});
